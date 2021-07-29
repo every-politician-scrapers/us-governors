d7a229857a442a28fca650167bf721cff1df9e18
Tony Bowden <tony@tmtm.com>
2021-07-29 08:51:36 +0300

    Remove 'United States' before comparison
    Don't complain about "United States Virgin Islands" vs "Virgin Islands",
    for example

--- bin/diff.rb
+++ bin/diff.rb
@@ -3,5 +3,15 @@
 
 require 'every_politician_scraper/comparison'
 
-diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+class UnitedStatesComparison < EveryPoliticianScraper::Comparison
+  def wikidata_csv_options
+    { converters: [->(v) { v.to_s.gsub(/^United States /, '') }] }
+  end
+
+  def external_csv_options
+    { converters: [->(v) { v.to_s.gsub(/^United States /, '') }] }
+  end
+end
+
+diff = UnitedStatesComparison.new('data/wikidata.csv', 'data/official.csv').diff
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
