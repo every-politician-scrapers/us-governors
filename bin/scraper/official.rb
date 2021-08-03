4a28a63e35d7aaf42d42524e9483525275847aaf
Tony Bowden <tony@tmtm.com>
2021-08-03 09:22:15 +0300

    Switch to PR rather than direct commit

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -2,7 +2,6 @@
 # frozen_string_literal: true
 
 require 'every_politician_scraper/scraper_data'
-require 'open-uri/cached'
 require 'pry'
 
 class MemberList
@@ -32,5 +31,5 @@ class MemberList
   end
 end
 
-url = 'https://www.nga.org/governors/'
-puts EveryPoliticianScraper::ScraperData.new(url).csv
+file = Pathname.new 'html/official.html'
+puts EveryPoliticianScraper::FileData.new(file).csv
